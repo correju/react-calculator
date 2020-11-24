74cf09a555aec92060fa913543e4fecc0353a3ba
Julian <julian@bunnystudio.com>
2020-11-23 21:21:51 -0500

    create action

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,14 +1,16 @@
 import Panel from '../Panel'
 import './index.css'
 import {connect} from 'react-redux'
+import calculateAction from '../../redux/actions/calculateAction'
 import './index.css'
 
 const mapStateToProps = state => state.calculate
 
 const Calculator = ({value, dispatch}) => {
   const clickHandler = (buttonName) => {
-    dispatch({type: buttonName})
+    dispatch(calculateAction(buttonName))
   }
+
   return <div className="container">
     <div className="display">{value.toString() === "Infinity" ?  "Err" : value}</div>
     <Panel clickHandler={clickHandler} />
---
+++ src/redux/actions/calculateAction.js
@@ -0,0 +1,5 @@
+const { default: defaultValue } = require("../../helpers/defaultValue")
+
+const calculateAction = (type) => ({ type })
+
+export default calculateAction
